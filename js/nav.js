405c4b79536212647da3a8de3ac8311d4da53c3c
ProjXDev <61373609+ProjXDev@users.noreply.github.com>
2020-10-08 15:10:58 +0200

    Update nav.js

--- js/nav.js
+++ js/nav.js
@@ -1,20 +1,20 @@
 function ajax_get(url, callback) {
-  var xmlhttp = new XMLHttpRequest();
-  xmlhttp.onreadystatechange = function () {
-      if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
 
-          try {
-              var data = xmlhttp.responseText;
-          } catch (err) {
-              console.log(err.message + " in " + xmlhttp.responseText);
-              return;
-          }
-          callback(data);
-      }
-  };
+            try {
+                var data = xmlhttp.responseText;
+            } catch (err) {
+                console.log(err.message + " in " + xmlhttp.responseText);
+                return;
+            }
+            callback(data);
+        }
+    };
 
-  xmlhttp.open("GET", url, true);
-  xmlhttp.send();
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
 }
 
 var currentPath = window.location.pathname;
@@ -25,49 +25,67 @@ var currentPath = window.location.pathname;
 var partialsCache = {}
 
 function resolvePathAndTitle(path) {
-  switch (path) {
-      case '/':
-          return {
-              htmlPath: '/html/home.html', title: 'Home'
-          }
-          case '/Flip-Challenge':
-              return {
-                  htmlPath: '/html/flip-challenge.html', title: 'Flip Challenge'
-              }
-              case '/admin':
+    switch (path) {
+        case '/':
+            return {
+                htmlPath: '/html/home.html', title: 'Home'
+            }
+            break;
+        case '/Flip-Challenge':
+            return {
+                htmlPath: '/html/flip-challenge.html', title: 'Flip Challenge'
+            }
+            break;
+        case '/admin':
+            return {
+                htmlPath: '/html/admin.html', title: 'admin'
+            }
+            break;
+        case '/invite':
+            return {
+                htmlPath: '/html/invite.html', title: 'invite'
+            }
+            break;
+            case '/profile':
                 return {
-                    htmlPath: '/html/admin.html', title: 'admin'
+                    htmlPath: '/html/profile.html', title: 'profile'
                 }
-          default:
-              return {
-                  htmlPath: '/html/home.html', title: 'Home'
-              }
-  }
+                break;
+                case '/quiz':
+            return {
+                htmlPath: '/html/quiz.html', title: 'quiz'
+            }
+            break;
+        default:
+            return {
+                htmlPath: '/html/home.html', title: 'Home'
+            }
+    }
 }
 
 function navigate(path) {
-  let PF = path.split('/')[1];
-  if (partialsCache[PF]) {
+    let PF = path.split('/')[1];
+    if (partialsCache[PF]) {
 
-      document.getElementById("content").innerHTML = partialsCache[PF];
-      document.title = resolvePathAndTitle(path).title;
-      window.history.pushState({
-          "html": partialsCache[PF],
-          "pageTitle": resolvePathAndTitle(path).title
-      }, "", path);
-      return
-  } else {
+        document.getElementById("content").innerHTML = partialsCache[PF];
+        document.title = resolvePathAndTitle(path).title;
+        window.history.pushState({
+            "html": partialsCache[PF],
+            "pageTitle": resolvePathAndTitle(path).title
+        }, "", path);
+        return
+    } else {
 
-      ajax_get(resolvePathAndTitle(path).htmlPath, function (data) {
-          partialsCache[PF] = data;
-          document.getElementById("content").innerHTML = data;
-          document.title = resolvePathAndTitle(path).title;
-          window.history.pushState({
-              "html": data,
-              "pageTitle": resolvePathAndTitle(path).title
-          }, "", path);
-      })
-  }
+        ajax_get(resolvePathAndTitle(path).htmlPath, function (data) {
+            partialsCache[PF] = data;
+            document.getElementById("content").innerHTML = data;
+            document.title = resolvePathAndTitle(path).title;
+            window.history.pushState({
+                "html": data,
+                "pageTitle": resolvePathAndTitle(path).title
+            }, "", path);
+        })
+    }
 
 
 }
@@ -75,8 +93,8 @@ navigate(currentPath);
 
 
 window.onpopstate = function (e) {
-  if (e.state) {
-      document.getElementById("content").innerHTML = e.state.html;
-      document.title = e.state.pageTitle;
-  }
+    if (e.state) {
+        document.getElementById("content").innerHTML = e.state.html;
+        document.title = e.state.pageTitle;
+    }
 };
