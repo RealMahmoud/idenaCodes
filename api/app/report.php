7853fd7838adb2b7fc99a3ed132ca3effec7621c
ProjXDev <61373609+ProjXDev@users.noreply.github.com>
2020-10-13 11:28:12 +0200

    add reporting ability

---
+++ api/app/report.php
@@ -0,0 +1,37 @@
+<?php
+session_start();
+include(dirname(__FILE__)."/../../common/_public.php");
+header('Content-Type: application/json');
+$result = (object)array();
+if(isset($_SESSION['CODES-Token'])) {
+    $loggedUserAddress = $conn->query("SELECT address FROM `auth_idena` where `token` = '".$_SESSION['CODES-Token']."' AND `authenticated` = '1'  LIMIT 1 ;")->fetch_row()[0];
+}else{
+  $result->error=true;
+  die(json_encode($result));
+}
+
+if ($loggedUserAddress == null) {
+    $result->error=true;
+    die(json_encode($result));
+} else {
+    $loggedUserID = $conn->query("SELECT id FROM `users` where `address` = '".$loggedUserAddress."' LIMIT 1 ;")->fetch_row()[0];
+}
+
+if(!isset($_POST['forID']) || !isset($_POST['report'])){
+  $result->error=true;
+  die(json_encode($result));
+}
+$forID = htmlspecialchars($conn->real_escape_string($_POST['forID']));
+$report = htmlspecialchars($conn->real_escape_string($_POST['report']));
+$forID = (int)$forID;
+
+
+
+
+$conn->query("INSERT INTO `reports_tickets`( `userID`, `report`, `reporterID`) VALUES ('".$loggedUserID."','".$report."','".$forID."');");
+
+
+
+
+$result->error=false;
+echo json_encode($result);
