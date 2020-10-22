0368b8e9098c46176aa5443fc7be634ccdbd461c
ProjXDev <61373609+ProjXDev@users.noreply.github.com>
2020-10-22 15:12:16 +0200

    add creationDate for telegram

--- api/telegram/auth.php
+++ api/telegram/auth.php
@@ -8,6 +8,22 @@ die('Already exist');
 }
 $keys = array("id", "first_name", "last_name", "username", "photo_url", "auth_date");
 
+function getClosest($search, $object) {
+   
+	$closest = null;
+	foreach ($object as  $key=>$xx)  {
+       
+	   if ($closest === null || abs($search - $closest) > abs($key - $search)) {
+          $closest = $key;
+          
+	   }
+	}
+	return $object[$closest];
+ }
+
+
+ $dates = file_get_contents("./dates.json");
+$datesArr = json_decode($dates, true); 
 
 if (isset($_GET['hash'])) {
 	foreach ($_GET as $key => $value) {
@@ -24,12 +40,13 @@ if (isset($_GET['hash'])) {
 		die($_GET['auth_date']);
 	  }
 	if (hash_equals($hash, $_GET['hash'])) {
-		$conn->query("INSERT INTO `auth_telegram`( `userID`, `tg_ID`, `tg_Username`, `tg_photo_url`, `time`) VALUES (
+		$conn->query("INSERT INTO `auth_telegram`( `userID`, `tg_ID`, `tg_Username`, `tg_photo_url`, `time`,`creationDate`) VALUES (
 			(SELECT id FROM `users` WHERE address = (SELECT address FROM `auth_idena` WHERE token = '".$_SESSION['CODES-Token']."')),
 			'".$_GET['id']."',
 			'".$_GET['username']."',
 			'".$_GET['photo_url']."',
-			'".$_GET['auth_date']."'
+			'".$_GET['auth_date']."',
+			'".getClosest($_GET['id'],$datesArr)."'
 		);");
 		echo "Telegram data integrity check success!";
 	}else{
