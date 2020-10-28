aceade7f157076ced5bd55934ed3d56f1c03d694
ProjXDev <61373609+ProjXDev@users.noreply.github.com>
2020-10-28 21:56:51 +0200

    Update auth.php

--- api/discord/auth.php
+++ api/discord/auth.php
@@ -1,6 +1,13 @@
 <?php
 
 include_once(dirname(__FILE__)."/../../common/_public.php");
+if (isset($_SESSION['CODES-Token'])) {
+    $loggedUserID = $conn->query("SELECT id FROM `users` where `address` = (SELECT address FROM `auth_idena` where `token` = '".$_SESSION['CODES-Token']."' AND `authenticated` = '1' ) LIMIT 1 ;")->fetch_row()[0];
+} else {
+    $result = (object)array();
+    $result->error=true;
+    die(json_encode($result));
+}
 
  $state = $_SESSION['state'] = bin2hex(openssl_random_pseudo_bytes(12));
  header('location '.'https://discordapp.com/oauth2/authorize?response_type=code&client_id=' . DISCORD_CLIENT . '&redirect_uri=' . DISCORD_CALLBACK . '&scope=' . DISCORD_SCOPE . "&state=" . $state);
