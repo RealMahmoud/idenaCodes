e6f6f6ac28eeedcc69a1f028d9f757d96fae6341
ProjXDev <61373609+ProjXDev@users.noreply.github.com>
2020-10-29 12:27:31 +0200

    Update _public.php

--- common/_public.php
+++ common/_public.php
@@ -1,16 +1,18 @@
 <?php
 
-// Rename this file before deploying
 ini_set('display_startup_errors', 1);
 ini_set('display_errors', 1);
 error_reporting(-1);
 
-$url = 'http://127.0.0.1/';
-$site_name = 'Idena codes';
+
+$site_name = 'Idena Codes';
+$telegramToken= '';
 $servername = "localhost";
-$username = "root";
+$username = "";
 $password = "";
 $dbname = "idenacodes";
+
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
@@ -35,3 +37,13 @@ function GUID()
     }
     return sprintf('%04X%04X-%04X-%04X-%04X-%04X%04X%04X', mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(16384, 20479), mt_rand(32768, 49151), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535));
 }
+
+
+define('DISCORD_CLIENT', '');
+define('DISCORD_SECRET', '');
+define('DISCORD_CALLBACK', 'http://codes.localhost/api/discord/check.php');
+define('DISCORD_SCOPE', 'identify');
+
+define('TWITTER_KEY', '');
+define('TWITTER_SECRET', '');
+define('TWITTER_CALLBACK', 'http://codes.localhost/api/twitter/check.php');
