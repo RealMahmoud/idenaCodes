a30fbcd649de0b18f4aa56485a56bcd041cfd925
RealMahmoud <RealMahmoudm@gmail.com>
2020-11-12 18:54:09 +0200

    add banning

--- api/account/balance.php
+++ api/account/balance.php
@@ -1,35 +1,34 @@
 <?php
 session_start();
-include(dirname(__FILE__)."/../../common/_public.php");
+include dirname(__FILE__) . "/../../common/_public.php";
 header('Content-Type: application/json');
 
-
 if (isset($_SESSION['CODES-Token'])) {
-    $loggedUserID = $conn->query("SELECT id FROM `users` where `address` = (SELECT address FROM `auth_idena` where `token` = '".$_SESSION['CODES-Token']."' AND `authenticated` = '1' ) LIMIT 1 ;")->fetch_row()[0];
+    $data = $conn->query("SELECT `id`,`banned` FROM `users` where `address` = (SELECT address FROM `auth_idena` where `token` = '" . $_SESSION['CODES-Token'] . "' AND `authenticated` = '1' ) LIMIT 1 ;")->fetch_row();
+    $loggedUserID = $data[0];
+    $banned = $data[1];
+    if ($banned) {
+        $result = (object) array();
+        $result->error = true;
+        die(json_encode($result));
+    }
 } else {
-    /*  $result = (object)array();
-      $result->error=true;
-      die(json_encode($result)); */
-    $loggedUserID = 9;
+    $result = (object) array();
+    $result->error = true;
+    die(json_encode($result));
 }
 
+$result = (object) array();
 
-
-$result = (object)array();
-
-$row =  $conn->query("SELECT balance FROM users where id = '".$loggedUserID."' LIMIT 1;")->fetch_row();
+$row = $conn->query("SELECT balance FROM users where id = '" . $loggedUserID . "' LIMIT 1;")->fetch_row();
 
 if ($row == null) {
-    $result->error=true;
+    $result->error = true;
     echo json_encode($result);
 } else {
-    $result->error=false;
+    $result->error = false;
 
-    $result->balance=$row[0];
-
-
-
-    
+    $result->balance = $row[0];
 
     echo json_encode($result);
 }
--- api/quiz/submitAnswers.php
+++ api/quiz/submitAnswers.php
@@ -4,10 +4,17 @@ include(dirname(__FILE__) . "/../../common/_public.php");
 header('Content-Type: application/json');
 
 if (isset($_SESSION['CODES-Token'])) {
-    $loggedUserID = $conn->query("SELECT id FROM `users` where `address` = (SELECT address FROM `auth_idena` where `token` = '".$_SESSION['CODES-Token']."' AND `authenticated` = '1' ) LIMIT 1 ;")->fetch_row()[0];
+    $data = $conn->query("SELECT `id`,`banned` FROM `users` where `address` = (SELECT address FROM `auth_idena` where `token` = '" . $_SESSION['CODES-Token'] . "' AND `authenticated` = '1' ) LIMIT 1 ;")->fetch_row();
+    $loggedUserID = $data[0];
+    $banned = $data[1];
+    if ($banned) {
+        $result = (object) array();
+        $result->error = true;
+        die(json_encode($result));
+    }
 } else {
-    $result = (object)array();
-    $result->error=true;
+    $result = (object) array();
+    $result->error = true;
     die(json_encode($result));
 }
 $answers = json_decode(file_get_contents('php://input'), true);
