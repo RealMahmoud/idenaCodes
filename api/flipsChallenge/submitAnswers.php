a86532a48fdbf42c393dbe66ec77f0eba5b56a0e
RealMahmoud <RealMahmoudm@gmail.com>
2020-11-15 22:37:00 +0200

    Update submitAnswers.php

--- api/flipsChallenge/submitAnswers.php
+++ api/flipsChallenge/submitAnswers.php
@@ -22,28 +22,34 @@ if (isset($_SESSION['CODES-Token'])) {
 
 $answers = json_decode(file_get_contents('php://input'), true);
 
-$resultSQL = $conn->query("SELECT flips FROM `test_flips` WHERE userID = '" . $loggedUserID . "' LIMIT 1;")->fetch_assoc()["flips"];
-
-$rightAnswers = 0;
-$totalFlips = 0;
-foreach (json_decode($resultSQL) as $flipID) {
-    $answer = $conn->query("SELECT answer FROM `flips` WHERE id = '" . $flipID . "' LIMIT 1;")->fetch_assoc()['answer'];
-
-    if (!is_bool(array_search($flipID, array_column($answers, 'id')))) {
-        if ($answer == $answers[array_search($flipID, array_column($answers, 'id'))]["answer"]) {
-            $rightAnswers += 1;
+$oldFlips = $conn->query("SELECT `flips`,`score` FROM `test_flips` WHERE userID = '" . $loggedUserID . "' LIMIT 1;")->fetch_assoc();
+if ($oldFlips) {
+    if (isset($oldFlips[1])) {
+        $result = (object) array();
+        $result->error = true;
+        $result->reason = "Already submitted";
+        die(json_encode($result));
+    } else {
+        $rightAnswers = 0;
+        $totalFlips = 0;
+        foreach (json_decode($oldFlips[0]) as $flipID) {
+            $answer = $conn->query("SELECT answer FROM `flips` WHERE id = '" . $flipID . "' LIMIT 1;")->fetch_assoc()['answer'];
+            if (!is_bool(array_search($flipID, array_column($answers, 'id')))) {
+                if ($answer == $answers[array_search($flipID, array_column($answers, 'id'))]["answer"]) {
+                    $rightAnswers += 1;
+                }
+            }
+            $totalFlips += 1;
         }
+        (int) $score = (float) ($rightAnswers / $totalFlips) * 100;
+        $conn->query("UPDATE `test_flips` SET `score`= '" . $score . "' WHERE `userID` = '" . $loggedUserID . "' LIMIT 1;");
+        $result = (object) array();
+        $result->error = false;
+        $result->score = $score;
+        die(json_encode($result));
     }
-
-    $totalFlips += 1;
+} else {
+    $result = (object) array();
+    $result->error = true;
+    die(json_encode($result));
 }
-
-(int) $score = (float) ($rightAnswers / $totalFlips) * 100;
-
-$conn->query("UPDATE `test_flips` SET `score`= '" . $score . "' WHERE `userID` = '" . $loggedUserID . "' LIMIT 1;");
-
-$result = (object) array();
-$result->error = false;
-$result->score = $score;
-
-echo json_encode($result);
