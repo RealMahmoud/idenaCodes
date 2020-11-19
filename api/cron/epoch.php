e14e88a69d730ba0781911c2ed3774c9015b9e6a
RealMahmoud <RealMahmoudm@gmail.com>
2020-11-20 01:47:04 +0200

    Update epoch.php

--- api/cron/epoch.php
+++ api/cron/epoch.php
@@ -1,5 +1,4 @@
 <?php
-session_start();
 include dirname(__FILE__) . "/../../common/_public.php";
 
 if (!isset($argv[1])) {
@@ -9,18 +8,17 @@ if (!$argv[1] == CRON_PASSWORD) {
     die("");
 }
 
-$epochArray = array(
+$epochArray = json_encode(array(
     "method" => "dna_epoch",
     "params" => [],
     "id" => 1,
     "key" => RPC_KEY
-);
-$result = curl_get(RPC_BASE_URL, $epochArray);
+));
+$result = curl_post(RPC_BASE_URL, $epochArray);
 if (isset($result['result'])) {
     $result = $result['result'];
     $conn->query("UPDATE `config` SET `value` = " . $result['epoch'] . " WHERE `key` = 'epoch';");
-    $conn->query("UPDATE `config` SET `value` = " . $result['minScoreForInvite'] . " WHERE `key` = 'minScoreForInvite';");
-    $conn->query("UPDATE `config` SET `value` = " . $result['validationTime'] . " WHERE `key` = 'validationTime';");
+    $conn->query("UPDATE `config` SET `value` = " . $result['nextValidation'] . " WHERE `key` = 'validationTime';");
     die("Success");
 } else {
     die("ERROR");
