258ae39ad0d45fb87b5c4f996dd85806918d890f
RealMahmoud <RealMahmoudm@gmail.com>
2020-11-20 10:15:57 +0200

    fixes

--- api/app/checkID.php
+++ api/app/checkID.php
@@ -4,11 +4,10 @@ include dirname(__FILE__) . "/../../common/_public.php";
 header('Content-Type: application/json');
 $result = (object) array();
 if (isset($_SESSION['CODES-Token'])) {
-    $data = $conn->query("SELECT `id`,`banned` FROM `users` where `address` = (SELECT `address` FROM `auth_idena` where `token` = '" . $_SESSION['CODES-Token'] . "' AND `authenticated` = '1' ) LIMIT 1 ;")->fetch_row();
+    $data = $conn->query("SELECT `id`,`banned` FROM `users` WHERE `address` = (SELECT `address` FROM `auth_idena` WHERE `token` = '" . $_SESSION['CODES-Token'] . "' AND `authenticated` = '1' ) LIMIT 1 ;")->fetch_row();
     $loggedUserID = $data[0];
     $banned = $data[1];
     if ($banned) {
-
         $result->error = true;
         $result->reason = "Banned";
         die(json_encode($result));
@@ -24,7 +23,7 @@ $id = $conn->real_escape_string($_GET['id']);
 $id = htmlspecialchars($id);
 $id = (int) $id;
 
-$row = $conn->query("SELECT `id`,`status`,`joined`,`lastseen`,`flag`,`ip`,`country`,`type` FROM `users` where `id` = '" . $id . "' LIMIT 1;")->fetch_row();
+$row = $conn->query("SELECT `id`,`status`,`joined`,`lastseen`,`flag`,`ip`,`country`,`type` FROM `users` WHERE `id` = '" . $id . "' LIMIT 1;")->fetch_row();
 
 if ($row == null) {
     $result->error = true;
@@ -39,7 +38,7 @@ if ($row == null) {
     $result->flag = $row[4];
     $result->score = 0;
     if (isset($row[5])) {
-        $result->ipCount = $conn->query("SELECT COUNT(*) FROM `users` where `ip` = '" . $row[6] . "' ;")->fetch_row()[0];
+        $result->ipCount = $conn->query("SELECT COUNT(*) FROM `users` WHERE `ip` = '" . $row[6] . "' ;")->fetch_row()[0];
         $result->score = $result->score + 10;
     } else {
         $result->ipCount = ' - ';
@@ -51,30 +50,32 @@ if ($row == null) {
         $result->country = ' - ';
     }
 
-    /*$result->boughtCount = $conn->query("SELECT COUNT(*) FROM `bought_users` where `boughtUserID` = '" . $id . "' ;")->fetch_row()[0];
-    $result->buyCount = $conn->query("SELECT COUNT(*) FROM `bought_users` where `userID` = '" . $id . "' ;")->fetch_row()[0];*/
+    /*$result->boughtCount = $conn->query("SELECT COUNT(*) FROM `bought_users` WHERE `boughtUserID` = '" . $id . "' ;")->fetch_row()[0];
+    $result->buyCount = $conn->query("SELECT COUNT(*) FROM `bought_users` WHERE `userID` = '" . $id . "' ;")->fetch_row()[0];*/
 
-    $result->reports = $conn->query("SELECT COUNT(*) FROM `reports` where `userID` = '" . $id . "' ;")->fetch_row()[0];
+    $result->reports = $conn->query("SELECT COUNT(*) FROM `reports` WHERE `userID` = '" . $id . "' ;")->fetch_row()[0];
 
-    if ($row[7] == 0) {
+
+    $loggedUserType = (int)$conn->query("SELECT `type` FROM `users` WHERE `id` = '" . $loggedUserID . "' ;")->fetch_row()[0];
+    if ($loggedUserType == 0) {
         $result->inviteAbility = false;
         $result->votingAbility = false;
-    } elseif ($row[7] == 1) {
+    } elseif ($loggedUserType == 1) {
         $result->inviteAbility = true;
         $result->votingAbility = true;
-    } elseif ($row[7] == 2) {
+    } elseif ($loggedUserType == 2) {
         $result->inviteAbility = true;
         $result->votingAbility = true;
     } else {
         $result->inviteAbility = false;
         $result->votingAbility = false;
     }
-    if ($id = $loggedUserID) {
+    if ($id == $loggedUserID) {
         $result->inviteAbility = false;
         $result->votingAbility = false;
     }
-    $countUp = $conn->query("SELECT COUNT(*) FROM `votes` where `forID` = '" . $id . "' AND `type` =  1;")->fetch_row()[0];
-    $countDown = $conn->query("SELECT COUNT(*) FROM `votes` where `forID` = '" . $id . "' AND `type` =  0;")->fetch_row()[0];
+    $countUp = $conn->query("SELECT COUNT(*) FROM `votes` WHERE `forID` = '" . $id . "' AND `type` =  1;")->fetch_row()[0];
+    $countDown = $conn->query("SELECT COUNT(*) FROM `votes` WHERE `forID` = '" . $id . "' AND `type` =  0;")->fetch_row()[0];
     $votesCount = (int) $countUp - (int) $countDown;
     if (isset($votesCount)) {
         $result->votes = $votesCount;
@@ -82,7 +83,7 @@ if ($row == null) {
         $result->votes = 0;
     }
 
-    $quizScore = $conn->query("SELECT `score` FROM `test_questions` where `userID` = '" . $id . "';")->fetch_row();
+    $quizScore = $conn->query("SELECT `score` FROM `test_questions` WHERE `userID` = '" . $id . "';")->fetch_row();
     if (isset($quizScore[0])) {
         $result->quizScore = $quizScore[0] . '%';
         $result->score = $result->score + 20;
@@ -90,7 +91,7 @@ if ($row == null) {
         $result->quizScore = ' - ';
     }
 
-    $flipChallengeScore = $conn->query("SELECT `score` FROM `test_flips` where `userID` = '" . $id . "';")->fetch_row();
+    $flipChallengeScore = $conn->query("SELECT `score` FROM `test_flips` WHERE `userID` = '" . $id . "';")->fetch_row();
     if (isset($flipChallengeScore[0])) {
         $result->flipChallengeScore = $flipChallengeScore[0] . ' %';
         $result->score = $result->score + 20;
@@ -99,7 +100,7 @@ if ($row == null) {
     }
 
     $accounts = array();
-    $tgResult = $conn->query("SELECT `id`,`tg_creationDate` FROM `auth_telegram` where `userID` = '" . $id . "' LIMIT 1;")->fetch_row();
+    $tgResult = $conn->query("SELECT `id`,`tg_creationDate` FROM `auth_telegram` WHERE `userID` = '" . $id . "' LIMIT 1;")->fetch_row();
     if (isset($tgResult[0])) {
         $service = (object) array();
         $service->name = 'Telegram';
@@ -108,7 +109,7 @@ if ($row == null) {
         array_push($accounts, $service);
         $result->score = $result->score + 10;
     }
-    $dcResult = $conn->query("SELECT `id`,dc_creationDate FROM `auth_discord` where `userID` = '" . $id . "' LIMIT 1;")->fetch_row();
+    $dcResult = $conn->query("SELECT `id`,dc_creationDate FROM `auth_discord` WHERE `userID` = '" . $id . "' LIMIT 1;")->fetch_row();
     if (isset($dcResult[0])) {
         $service = (object) array();
         $service->name = 'Discord';
@@ -117,7 +118,7 @@ if ($row == null) {
         array_push($accounts, $service);
         $result->score = $result->score + 20;
     }
-    $twResult = $conn->query("SELECT `id`,tw_creationDate FROM `auth_twitter` where `userID` = '" . $id . "' LIMIT 1;")->fetch_row();
+    $twResult = $conn->query("SELECT `id`,tw_creationDate FROM `auth_twitter` WHERE `userID` = '" . $id . "' LIMIT 1;")->fetch_row();
     if (isset($twResult[0])) {
         $service = (object) array();
         $service->name = 'Twitter';
